62b8f5641de53617da02511c7d7de4f03147b8bd
Valentin Bellamy <bellamyvalentin@gmail.com>
2019-12-11 15:57:58 +0100

    first commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,13 +8,8 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
+    <span>WELCOME</span>
+    <h1>I'm a french frontend developer, working on his portfolio !</h1>
   </Layout>
 )
 
